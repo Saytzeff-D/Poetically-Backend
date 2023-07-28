b2fc103e7811d2970ce044aad37da6934748ac32
David Atanda <ololadedavid15@gmail.com>
2023-07-28 11:03:25 +0100

    server configuration

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const pool = require('./pool');
+const userRouter = require('./routes/user.route');
+app.use(cors({origin: '*'}))
+app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}))
+app.use(bodyParser.json({limit:'50mb'}));
+app.use('/user', userRouter)
+
+app.get('/', (req, res)=>{
+    res.send('Poetically-Me Server is now live')
+})
+
+pool.getConnection((err, conn)=>{
+    if (!err) {
+        console.log('This server is now connected to a SQL Database')
+    } else {
+        console.log('Error in Connection')
+    }
+})
+app.listen(process.env.PORT, (req, res)=>{
+    console.log(`Poetically-Me Server is now listening on Port ${process.env.PORT}`)
+})
