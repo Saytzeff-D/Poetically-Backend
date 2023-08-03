3b6f72f49566236e5ef26d60357c8e3c60b0ee39
David Atanda <ololadedavid15@gmail.com>
2023-08-03 16:14:16 +0100

    shop route

--- routes/poem.route.js
+++ routes/poem.route.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { uploadPoem, myPoems, setVisibility } = require('../controllers/poem.controller')
+const { uploadPoem, myPoems, setVisibility, myShop } = require('../controllers/poem.controller')
 const { authJWT } = require('../middlewares/jwt.middleware')
 const PoemRouter = express.Router()
 
 PoemRouter.post('/upload', authJWT, uploadPoem)
 PoemRouter.get('/my-poems', authJWT, myPoems)
 PoemRouter.put('/setVisibility', authJWT, setVisibility)
+PoemRouter.get('/my-shop', authJWT, myShop)
 
 module.exports = PoemRouter
